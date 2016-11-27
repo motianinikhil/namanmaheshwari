fc6fe53447b029d6c73d20c55f3028dd94e4456f
Nikhil Motiani <nikhil.m@247-inc.com>
2016-11-28 02:25:13 +0530

    Final Draft

--- js/initData.js
+++ js/initData.js
@@ -13,8 +13,7 @@ var DATA = {
           "title": "Texas Instruments, India (Jul 2015 - Present)",
           "messageHead": "<strong>Digital Design Engineer, Design-for-Testability Team, Automotive-Radar chip</strong>",
           "messageList": [
-            "Implemented the Built-In Self-Test (BIST) mechanism for memories for a complex design with 250+ memories <ul class='lowspace'><li>Implemented the architecture for memory testing during production as well as field testing",
-            "Verification of multiple fault-detection algorithms on all the memories in the design across multiple process corners, with high-security considerations and test-time optimization techniques like concurrent testing of memories</li></ul>",
+            "Implemented the Built-In Self-Test (BIST) mechanism for memories for a complex design with 250+ memories <ul class='lowspace'><li>Implemented the architecture for memory testing during production as well as field testing</li><li>Verification of multiple fault-detection algorithms on all the memories in the design across multiple process corners, with high-security considerations and test-time optimization techniques like concurrent testing of memories</li></ul>",
             "Generated test patterns for advanced fault models like Small Delay Defect (SDD) & Path Delay, and measured the test quality by fault simulating across various fault models, thus improving test quality and optimizing pattern count",
             "Automatic Test Pattern Generation (ATPG) for critical IPs in the design which are under the scope of Self-Test for automotive safety and implemented novel techniques like Low-Power Scan for scan-shift power reduction by 43%",
             "Invented and implemented a novel JTAG-based Hybrid Multiple Input Signature Register (H-MISR) which aids in seamless debug and diagnosis of MISR-based scan compression patterns without the need of exclusive long-chain mode"
@@ -85,7 +84,7 @@ var DATA = {
       "duration": "B.E. (Hons.), 2011-2015",
       "shortTitle": "Electrical & Electronics Engg, BITS-Pilani, India",
       "shortDesc": [
-        "<strong> CGPA: 9.31/10.0</strong><p>Birla Institute of Technology and Science (BITS)-Pilani, Pilani Campus, India <ul><li>Among the top 10 students in Electrical, Electronics & Instrumentation Departments in a batch of 150 students </li><li> Overall amongst top 5% students in BITS-Pilani</li></ul></p>"
+        "<strong> CGPA: 9.31/10.0</strong><p>Birla Institute of Technology and Science (BITS)-Pilani, Pilani Campus, India <ul><li  type='square'>Among the top 10 students in Electrical, Electronics & Instrumentation Departments in a batch of 150 students </li><li type='square'> Overall amongst top 5% students in BITS-Pilani</li></ul></p>"
       ]
     }
   ],
@@ -104,7 +103,7 @@ var DATA = {
 
       {
         "title": "Building a Virtual Lab",
-        "duration": "Aug 2014 - Dec 2014<br /><a href='./docs/VirtualLab.zip' class='pull-right'>Project Report</a>",
+        "duration": "Aug 2014 - Dec 2014<br /><div class='pull-right-lg'><a href='./docs/VirtualLab.zip'>Project Report</a></div>",
         "message": [
           "Built a Virtual Lab on open source software like Electric VLSI and Scilab for WILP students of BITS-Pilani"
         ],
@@ -160,7 +159,7 @@ var DATA = {
         "author": "<strong>Naman Maheshwari</strong>, Zhixi Yang, Jie Han and Fabrizio Lombardi",
         "popup": "",
         "footer": "Proceedings of 28th International Conference on VLSI Design, 2015 (<strong>VLSID-2015</strong>)" +
-                  "<br /><span class='glyphicon glyphicon-ok-circle'></span>  Awarded the <strong>Student Fellowship</strong> by the VLSID-2015 Conference Committee, for excellent record in academics & past work"
+                  "<br /><span class='fa fa-trophy'></span>  Awarded the <strong>Student Fellowship</strong> by the VLSID-2015 Conference Committee, for excellent record in academics & past work"
       },
       {
         "title": "Enhanced MISR Debug and Diagnosis with Per Cycle Scan Data Observation Capability",
@@ -174,7 +173,7 @@ var DATA = {
         "author": "Honglan Jiang, Cong Liu, <strong>Naman Maheshwari</strong>, Fabrizio Lombardi and Jie Han",
         "popup": "",
         "footer": "Proceedings of 12th ACM/IEEE International Symposium on Nanoscale Architectures, 2016 (<strong>NANOARCH-2016</strong>)" +
-                  "<br /><span class='glyphicon glyphicon-ok-circle'></span>  Nominated for the <strong>Best Paper Award</strong> in the conference proceedings"
+                  "<br /><span class='fa fa-trophy'></span>  Nominated for the <strong>Best Paper Award</strong> in the conference proceedings"
       },
       {
         "title": "Novel Debug and Diagnosis Method for MISR based Scan Compression Architecture",
@@ -197,9 +196,9 @@ var DATA = {
         "message": [
           "Invented a JTAG-based Hybrid MISR Implementation to solve the problem of debug in case of failing MISR-based scan compression patterns",
           "Various key features of the invention are<ul class='lowspace'>" +
-          "<li>Enables per pattern and per cycle signature observation without test-time and test-volume impact</li>" +
-          "<li>Existing Test Data Out (TDO) pin is used for MISR-Observe, avoiding the need for exclusive test-pin</li>" +
-          "<li>Does not require additional long-chain diagnostic test-mode and enables continue-on-fail in the event of failures</li>" +
+          "<li type='circle'>Enables per pattern and per cycle signature observation without test-time and test-volume impact</li>" +
+          "<li type='circle'>Existing Test Data Out (TDO) pin is used for MISR-Observe, avoiding the need for exclusive test-pin</li>" +
+          "<li type='circle'>Does not require additional long-chain diagnostic test-mode and enables continue-on-fail in the event of failures</li>" +
           "</ul>"
         ],
         "footer": "<strong>Approved</strong> for filing at USPTO from Texas Instruments "
@@ -207,170 +206,3 @@ var DATA = {
     ]
   }
 };
-
-
-function portfolio (obj, container) {
-  var head = '<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;"><ul class="resp-tabs-list wow fadeInUp animated" data-wow-delay=".7s">';
-  var body = '<div class="resp-tabs-container">';
-
-  function readPubs (currentTab, isProject) {
-    var content = '<ol class = "tab_img ">';
-    var firstDone = false;
-    for (var e = 0; e < currentTab.length; e++) {
-      var str = '<li><blockquote ';
-      var element = currentTab[e];
-      if (isProject) {
-        str += ' data-toggle="collapse" data-target="#project' + e + '" >';
-        str += element.title + '<span class="pull-right">' + element.duration;
-        if (element.report) {
-          str += ' <br /><a class="pull-right view-pdf" href="' + element.report + '" data-title="' + element.title +
-                 ' - Report">Project Report</a>';
-        }
-        str += '</span>';
-        element.footer = element.footer || "";
-        str += '<footer>' + element.footer + '<div id="project' + e + '" class="collapse in';
-        if (!firstDone) {
-          firstDone = true;
-        }
-        str += '">' + '<ul><li>' +
-               element.message.join("</li><li>") + '</li></ul>' +
-               '</div>' + '</footer>';
-        str += "</a></blockquote>";
-      } else {
-        str += '>' + element.author + ', <em><a ';
-        if (element.pdfSource) {
-          str += 'class="view-pdf" data-title="' + element.title + '" href="' + element.pdfSource + '" ';
-        } else {
-          element.popup = element.popup || "";
-          var ptitle = element.popup.title || "";
-          var pContent = element.popup.content || element.popup || element.tip || "Not Available for Viewing..." + "";
-          str += 'data-toggle="popover" data-content="' + pContent + '" title="' + ptitle + '"  data-placement="bottom"'
-        }
-        str += ' >' + element.title + '</a></em>';
-        if (element.footer) {
-          str += '<footer>' + element.footer + '</footer>';
-        }
-        str += "</blockquote>";
-      }
-      str += "</li>";
-      content += str;
-    }
-    content += '</ol>';
-    return content;
-  }
-
-  for (var tab in obj) {
-    if (obj.hasOwnProperty(tab)) {
-      head += '<li class="resp-tab-item"><span>' + tab + '</span></li>';
-      body += '<div class="tab-1 resp-tab-content"><div class="tab_img">';
-      body += readPubs(obj[tab], tab !== "PUBLICATIONS");
-      body += '</div></div>';
-    }
-  }
-  head += '</ul><div class="clearfix"></div>';
-  body += '</div>';
-
-  var portFolioContent = head + body + '</div>';
-
-  $(container).html(portFolioContent);
-
-}
-portfolio(DATA.PORTFOLIO, '#portfolio-container');
-$.ready($('[data-toggle="popover"]').popover());
-$.ready($('[data-toggle="tooltip"]').tooltip());
-
-function leftRight (obj, container, icon) {
-
-  if (!icon) {
-    icon = "glyphicon-briefcase";
-  }
-  var leftRightContent = "";
-  var firstDone = false;
-
-  var toggle1 = "work-left";
-  var toggle2 = "work-right";
-  var alternate = true;
-  for (var w in obj) {
-    var element = obj[w];
-    var content = '<div class="work-info">';
-    content += '<div class="col-md-6 ';
-    content += toggle1 + '">' +
-               '<h4>' + element.duration + '</h4>' +
-               '</div>' +
-               '<div class="col-md-6 ' + toggle2 + '"><h5>';
-    if (alternate) {
-      content += '<span class="glyphicon ' + icon + '"></span>';
-    }
-    content += element.shortTitle;
-    if (!alternate) {
-      content += '<span class="glyphicon ' + icon + '"></span>';
-    }
-    content += '</h5>';
-    for (d in element.shortDesc) {
-      var shortDesc = element.shortDesc[d];
-      content += '<p>' + shortDesc + '</p>';
-    }
-    if (element.additionalInfo) {
-      content +=
-        '<a class=" view-exp" data-ref="' + element.additionalInfo.dataRef + '">' + element.additionalInfo.linkText + '</a>';
-      if (!element.additionalInfo.data.message) {
-        element.additionalInfo.data.message =
-          "<strong>" + element.additionalInfo.data.messageHead + "</strong><br /><br /><ul class='lowspace'><li>" +
-          element.additionalInfo.data.messageList.join("</li><li>") + "</li></ul>";
-      }
-      window.workMap = window.workMap || {};
-      window.workMap[element.additionalInfo.dataRef] = element.additionalInfo.data;
-    }
-    content += '</div><div class="clearfix"></div></div>';
-
-    leftRightContent += content;
-    var temp = toggle1;
-    toggle1 = toggle2;
-    toggle2 = temp;
-    if (!firstDone) {
-      toggle1 = toggle1 + " " + toggle1 + "2";
-      toggle2 = toggle2 + " " + toggle2 + "2";
-      firstDone = true;
-    }
-    alternate = !alternate;
-
-  }
-  if (container) {
-    $(container).append(leftRightContent);
-  } else {
-    return leftRightContent;
-  }
-}
-leftRight(DATA.WORK, "#work-container", "glyphicon-briefcase");
-leftRight(DATA.EDUCATION, "#education-container", "glyphicon-education");
-$(document).ready(function () {
-  $('#submitContact').click(function (e) {
-    e.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: "http://formspree.io/naman.mah1993@gmail.com",
-      data: $('#contactForm').serialize(),
-      success: function () {
-        var successMsg = '<div id="alert-msg"  class="alert alert-success alert-dismissible fade in">' +
-                         '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
-                         'Message Sent Successfully' +
-                         '</div>';
-        $("#alert-msg").fadeTo(2000, 500).slideUp(500, function () {
-          $("#alert-msg").slideUp(500);
-        });
-
-        $('#alert').append(successMsg);
-      },
-      error: function () {
-        var failMsg = '<div id="alert-msg" class="alert alert-error alert-dismissible fade in">' +
-                      '<a href="#" class="close" data-dismiss="alert">&times;</a>' +
-                      '<strong> Error </strong>There was an error sending message. Please contact directly' +
-                      '</div>';
-        $('#alert').append(failMsg);
-      }
-    })
-  })
-});
-$(".alert-dismissable").fadeTo(2000, 500).slideUp(500, function () {
-  $(".alert-dismissable").alert('close');
-});
---
+++ js/processData.js
@@ -0,0 +1,167 @@
+
+function portfolio (obj, container) {
+  var head = '<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;"><ul class="resp-tabs-list wow fadeInUp animated" data-wow-delay=".7s">';
+  var body = '<div class="resp-tabs-container">';
+
+  function readPubs (currentTab, isProject) {
+    var content = '<ol class = "tab_img ">';
+    var firstDone = false;
+    for (var e = 0; e < currentTab.length; e++) {
+      var str = '<li><blockquote ';
+      var element = currentTab[e];
+      if (isProject) {
+        str += ' data-toggle="collapse" data-target="#project' + e + '" >';
+        str += element.title + '<div class="pull-right-lg">' + element.duration;
+        if (element.report) {
+          str += ' <br /><div class="pull-right-lg"><a class="view-pdf" href="' + element.report + '" data-title="' + element.title +
+                 ' - Report">Project Report</a></div>';
+        }
+        str += '</div>';
+        element.footer = element.footer || "";
+        str += '<footer>' + element.footer + '<div id="project' + e + '" class="collapse in';
+        if (!firstDone) {
+          firstDone = true;
+        }
+        str += '">' + '<ul><li  type="square">' +
+               element.message.join('</li><li type="square">') + '</li></ul>' +
+               '</div>' + '</footer>';
+        str += "</a></blockquote>";
+      } else {
+        str += '>' + element.author + ', <em><a ';
+        if (element.pdfSource) {
+          str += 'class="view-pdf" data-title="' + element.title + '" href="' + element.pdfSource + '" ';
+        } else {
+          element.popup = element.popup || "";
+          var ptitle = element.popup.title || "";
+          var pContent = element.popup.content || element.popup || element.tip || "Not Available for Viewing..." + "";
+          str += 'data-toggle="popover" data-content="' + pContent + '" title="' + ptitle + '"  data-placement="bottom"'
+        }
+        str += ' >' + element.title + '</a></em>';
+        if (element.footer) {
+          str += '<footer>' + element.footer + '</footer>';
+        }
+        str += "</blockquote>";
+      }
+      str += "</li>";
+      content += str;
+    }
+    content += '</ol>';
+    return content;
+  }
+
+  for (var tab in obj) {
+    if (obj.hasOwnProperty(tab)) {
+      head += '<li class="resp-tab-item"><span>' + tab + '</span></li>';
+      body += '<div class="tab-1 resp-tab-content"><div class="tab_img">';
+      body += readPubs(obj[tab], tab !== "PUBLICATIONS");
+      body += '</div></div>';
+    }
+  }
+  head += '</ul><div class="clearfix"></div>';
+  body += '</div>';
+
+  var portFolioContent = head + body + '</div>';
+
+  $(container).html(portFolioContent);
+
+}
+portfolio(DATA.PORTFOLIO, '#portfolio-container');
+$.ready($('[data-toggle="popover"]').popover());
+$.ready($('[data-toggle="tooltip"]').tooltip());
+
+function leftRight (obj, container, icon) {
+
+  if (!icon) {
+    icon = "glyphicon-briefcase";
+  }
+  var leftRightContent = "";
+  var firstDone = false;
+
+  var toggle1 = "work-left";
+  var toggle2 = "work-right";
+  var alternate = true;
+  for (var w in obj) {
+    var element = obj[w];
+    var content = '<div class="work-info">';
+    content += '<div class="col-md-6 ';
+    content += toggle1 + '">' +
+               '<h4>' + element.duration + '</h4>' +
+               '</div>' +
+               '<div class="col-md-6 ' + toggle2 + '"><h5>';
+    if (alternate) {
+      content += '<span class="glyphicon ' + icon + '"></span>';
+    }
+    content += element.shortTitle;
+    if (!alternate) {
+      content += '<span class="glyphicon ' + icon + '"></span>';
+    }
+    content += '</h5>';
+    for (d in element.shortDesc) {
+      var shortDesc = element.shortDesc[d];
+      content += '<p>' + shortDesc + '</p>';
+    }
+    if (element.additionalInfo) {
+      content +=
+        '<a class=" view-exp" data-ref="' + element.additionalInfo.dataRef + '">' + element.additionalInfo.linkText + '</a>';
+      if (!element.additionalInfo.data.message) {
+        element.additionalInfo.data.message =
+          "<strong>" + element.additionalInfo.data.messageHead +
+          "</strong><br /><br /><ul class='lowspace'><li type='square'>" +
+          element.additionalInfo.data.messageList.join("</li><li type='square'>") + "</li></ul>";
+      }
+      window.workMap = window.workMap || {};
+      window.workMap[element.additionalInfo.dataRef] = element.additionalInfo.data;
+    }
+    content += '</div><div class="clearfix"></div></div>';
+
+    leftRightContent += content;
+    var temp = toggle1;
+    toggle1 = toggle2;
+    toggle2 = temp;
+    if (!firstDone) {
+      toggle1 = toggle1 + " " + toggle1 + "2";
+      toggle2 = toggle2 + " " + toggle2 + "2";
+      firstDone = true;
+    }
+    alternate = !alternate;
+
+  }
+  if (container) {
+    $(container).append(leftRightContent);
+  } else {
+    return leftRightContent;
+  }
+}
+leftRight(DATA.WORK, "#work-container", "glyphicon-briefcase");
+leftRight(DATA.EDUCATION, "#education-container", "glyphicon-education");
+$(document).ready(function () {
+  $('#submitContact').click(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: "http://formspree.io/naman.mah1993@gmail.com",
+      data: $('#contactForm').serialize(),
+      success: function () {
+        var successMsg = '<div id="alert-msg"  class="alert alert-success alert-dismissible fade in">' +
+                         '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                         'Message Sent Successfully' +
+                         '</div>';
+        $("#alert-msg").fadeTo(2000, 500).slideUp(500, function () {
+          $("#alert-msg").slideUp(500);
+        });
+
+        $('#alert').append(successMsg);
+      },
+      error: function () {
+        var failMsg = '<div id="alert-msg" class="alert alert-error alert-dismissible fade in">' +
+                      '<a href="#" class="close" data-dismiss="alert">&times;</a>' +
+                      '<strong> Error </strong>There was an error sending message. Please contact directly' +
+                      '</div>';
+        $('#alert').append(failMsg);
+      }
+    })
+  })
+});
+$(".alert-dismissable").fadeTo(2000, 500).slideUp(500, function () {
+  $(".alert-dismissable").alert('close');
+});
